1xtAsh/cr_base

SystemUI: Implement RefreshRateManager [1/2]
Includes:
- Per-app refresh rate config
- Extreme refresh rate (Force all apps to run in maximum refresh rate)
- Temp refresh rate allowed (Used in MEMC)
- Refresh rate QS tile

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
[nurkeinneid: move to fwb and drop sdk extensions/dependencies]
Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

core/java/android/app/SystemServiceRegistry.java
@@ -290,6 +290,9 @@ import com.android.internal.os.IDropBoxManagerService;
 import com.android.internal.policy.PhoneLayoutInflater;
 import com.android.internal.util.Preconditions;
 
+import org.derpfest.display.IRefreshRateManagerService;
+import org.derpfest.display.RefreshRateManager;
+
 import java.time.InstantSource;
 import java.util.HashMap;
 import java.util.Map;
@@ -1823,6 +1826,15 @@ public final class SystemServiceRegistry {
                         return new IntrusionDetectionManager(service);
                     }
                 });
+
+        registerService(Context.REFRESH_RATE_MANAGER_SERVICE, RefreshRateManager.class,
+                new CachedServiceFetcher<RefreshRateManager>() {
+            @Override
+            public RefreshRateManager createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.REFRESH_RATE_MANAGER_SERVICE);
+                IRefreshRateManagerService service = IRefreshRateManagerService.Stub.asInterface(binder);
+                return new RefreshRateManager(ctx.getOuterContext(), service);
+            }});
 
         sInitializing = true;
         try {

core/java/com/android/internal/util/derpfest/DisplayRefreshRateHelper.java
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2022-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.derpfest;
+
+import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.PEAK_REFRESH_RATE;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.Display;
+
+import com.android.internal.R;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/** @hide */
+public class DisplayRefreshRateHelper {
+
+    private static final float DEFAULT_REFRESH_RATE = 60f;
+
+    private static DisplayRefreshRateHelper sInstance = null;
+
+    private final ArrayList<Integer> mRefreshRateList = new ArrayList<>();
+    private final Context mContext;
+
+    public static DisplayRefreshRateHelper getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new DisplayRefreshRateHelper(context);
+        }
+        return sInstance;
+    }
+
+    private DisplayRefreshRateHelper(Context context) {
+        mContext = context;
+        initialize();
+    }
+
+    private void initialize() {
+        final Display.Mode mode = mContext.getDisplay().getMode();
+        final Display.Mode[] modes = mContext.getDisplay().getSupportedModes();
+        for (Display.Mode m : modes) {
+            if (m.getPhysicalWidth() == mode.getPhysicalWidth() &&
+                    m.getPhysicalHeight() == mode.getPhysicalHeight()) {
+                mRefreshRateList.add((int) m.getRefreshRate());
+            }
+        }
+        mRefreshRateList.sort(Comparator.naturalOrder());
+    }
+
+    public ArrayList<Integer> getSupportedRefreshRateList() {
+        return mRefreshRateList;
+    }
+
+    public int getMinimumRefreshRate() {
+        final int refreshRate = mContext.getResources().getInteger(
+                R.integer.config_defaultRefreshRate);
+        final float defaultRefreshRate = refreshRate != 0 ? (float) refreshRate : DEFAULT_REFRESH_RATE;
+        final int ret = (int) Settings.System.getFloatForUser(mContext.getContentResolver(),
+                MIN_REFRESH_RATE, defaultRefreshRate, UserHandle.USER_SYSTEM);
+        if (mRefreshRateList.size() != 0 && !mRefreshRateList.contains(ret)) {
+            return mRefreshRateList.get(mRefreshRateList.size() - 1);
+        }
+        return ret;
+    }
+
+    public int getPeakRefreshRate() {
+        final int refreshRate = mContext.getResources().getInteger(
+                R.integer.config_defaultPeakRefreshRate);
+        final float defaultPeakRefreshRate = refreshRate != 0 ? (float) refreshRate : DEFAULT_REFRESH_RATE;
+        final int ret = (int) Settings.System.getFloatForUser(mContext.getContentResolver(),
+                PEAK_REFRESH_RATE, defaultPeakRefreshRate, UserHandle.USER_SYSTEM);
+        if (mRefreshRateList.size() != 0 && !mRefreshRateList.contains(ret)) {
+            return mRefreshRateList.get(mRefreshRateList.size() - 1);
+        }
+        return ret;
+    }
+
+    public ArrayList<Integer> getRefreshRate() {
+        final ArrayList<Integer> ret = new ArrayList<>();
+        ret.add(getMinimumRefreshRate());
+        ret.add(getPeakRefreshRate());
+        return ret;
+    }
+
+    public void setMinimumRefreshRate(int refreshRate) {
+        Settings.System.putFloatForUser(mContext.getContentResolver(),
+                MIN_REFRESH_RATE, (float) refreshRate, UserHandle.USER_SYSTEM);
+    }
+
+    public void setPeakRefreshRate(int refreshRate) {
+        Settings.System.putFloatForUser(mContext.getContentResolver(),
+                PEAK_REFRESH_RATE, (float) refreshRate, UserHandle.USER_SYSTEM);
+    }
+
+    public void setRefreshRate(int minRefreshRate, int peakRefreshRate) {
+        setMinimumRefreshRate(minRefreshRate);
+        setPeakRefreshRate(peakRefreshRate);
+    }
+
+    public boolean isRefreshRateValid(int refreshRate) {
+        return mRefreshRateList.contains(refreshRate);
+    }
+}

core/java/com/android/internal/util/derpfest/FullscreenTaskStackChangeListener.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.derpfest;
+
+import static android.app.ActivityTaskManager.INVALID_TASK_ID;
+import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
+
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.IActivityTaskManager;
+import android.app.TaskStackListener;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * TaskStackListener that ignores freeform / mini-window focuses change
+ *
+ * @hide
+ */
+public abstract class FullscreenTaskStackChangeListener extends TaskStackListener {
+
+    private static final String DEFAULT_TAG = "FullscreenTaskStackChangeListener";
+
+    private final ActivityManager mActivityManager;
+    private final ActivityTaskManager mActivityTaskManager;
+    private final IActivityTaskManager mIActivityTaskManager;
+
+    private final boolean mObserveActivityChange;
+
+    private boolean mDebug = false;
+    private String mDebugTag = DEFAULT_TAG;
+
+    private boolean mListening = false;
+
+    private String mTopPackage = "";
+    private String mTopActivity = "";
+    private int mTopTaskId = INVALID_TASK_ID;
+
+    public FullscreenTaskStackChangeListener(Context context) {
+        this(context, false);
+    }
+
+    public FullscreenTaskStackChangeListener(Context context, boolean observeActivity) {
+        mActivityManager = context.getSystemService(ActivityManager.class);
+        mActivityTaskManager = ActivityTaskManager.getInstance();
+        mIActivityTaskManager = ActivityTaskManager.getService();
+        mObserveActivityChange = observeActivity;
+    }
+
+    public void setDebug(boolean debug) {
+        mDebug = debug;
+    }
+
+    public void setDebugTag(String tag) {
+        mDebugTag = tag;
+    }
+
+    public void setListening(boolean listening) {
+        if (mListening != listening) {
+            if (listening) {
+                mActivityTaskManager.registerTaskStackListener(this);
+                mListening = true;
+            } else {
+                mActivityTaskManager.unregisterTaskStackListener(this);
+                mListening = false;
+            }
+        }
+    }
+
+    private void handleChange(String newPackageName, String newActivityName, int newTaskId) {
+        if (mTopPackage.equals(newPackageName) && mTopActivity.equals(newActivityName)) {
+            return;
+        }
+        mTopActivity = newActivityName;
+        if (!mObserveActivityChange && mTopPackage.equals(newPackageName)) {
+            return;
+        }
+        mTopPackage = newPackageName;
+        mTopTaskId = newTaskId;
+        if (mDebug) {
+            Log.d(mDebugTag, "Change: mTopPackage=" + mTopPackage
+                    + ", mTopActivity=" + mTopActivity
+                    + ", mTopTaskId=" + mTopTaskId);
+        }
+        onFullscreenTaskChanged(mTopPackage, mTopActivity, mTopTaskId);
+    }
+
+    @Override
+    public void onTaskStackChanged() {
+        forceCheck();
+    }
+
+    @Override
+    public void onTaskFocusChanged(int taskId, boolean focused) {
+        if (focused) {
+            forceCheck();
+        }
+    }
+
+    public void forceCheck() {
+        try {
+            final ActivityTaskManager.RootTaskInfo info = mIActivityTaskManager.getFocusedRootTaskInfo();
+            if (info == null) {
+                return;
+            }
+            if (info.getWindowingMode() == WINDOWING_MODE_FREEFORM) {
+                return;
+            }
+            final ComponentName topActivity = info.topActivity;
+            if (topActivity == null) {
+                return;
+            }
+            handleChange(topActivity.getPackageName(), topActivity.getClassName(), info.taskId);
+        } catch (RemoteException e) {}
+    }
+
+    public String getTopPackageName() {
+        return mTopPackage;
+    }
+
+    public String getTopActivityName() {
+        return mTopActivity;
+    }
+
+    public int getTopTaskId() {
+        return mTopTaskId;
+    }
+
+    public abstract void onFullscreenTaskChanged(
+            String packageName, String activityName, int taskId);
+}

core/java/org/derpfest/display/IRefreshRateListener.aidl
@@ -1,0 +1,14 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.display;
+
+/** @hide */
+oneway interface IRefreshRateListener {
+
+    void onRequestedRefreshRate(int refreshRate);
+
+    void onRequestedMemcRefreshRate(int refreshRate);
+}

services/java/com/android/server/SystemServer.java
@@ -332,6 +332,8 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+import com.android.server.DerpFestSystemExService;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -1715,6 +1717,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("WindowManagerServiceOnInitReady");
             wm.onInitReady();
+            t.traceEnd();
+
+            t.traceBegin("StartDerpFestSystemExService");
+            mSystemServiceManager.startService(DerpFestSystemExService.class);
             t.traceEnd();
 
             // Start receiving calls from SensorManager services. Start in a separate thread

chettoy/evox_frameworks_base

Change-Id: I1a5d4ccaf18389a0ea26342bd682856faa3ab17a
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

core/java/android/app/SystemServiceRegistry.java
@@ -290,6 +290,9 @@ import com.android.internal.os.IDropBoxManagerService;
 import com.android.internal.policy.PhoneLayoutInflater;
 import com.android.internal.util.Preconditions;
 
+import org.evolution.display.IRefreshRateManagerService;
+import org.evolution.display.RefreshRateManager;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -1834,6 +1837,15 @@ public final class SystemServiceRegistry {
                         return new IntrusionDetectionManager(service);
                     }
                 });
+
+        registerService(Context.REFRESH_RATE_MANAGER_SERVICE, RefreshRateManager.class,
+                new CachedServiceFetcher<RefreshRateManager>() {
+            @Override
+            public RefreshRateManager createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.REFRESH_RATE_MANAGER_SERVICE);
+                IRefreshRateManagerService service = IRefreshRateManagerService.Stub.asInterface(binder);
+                return new RefreshRateManager(ctx.getOuterContext(), service);
+            }});
 
         sInitializing = true;
         try {

core/java/com/android/internal/util/evolution/DisplayRefreshRateHelper.java
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2022-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.evolution;
+
+import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.PEAK_REFRESH_RATE;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.Display;
+
+import com.android.internal.R;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/** @hide */
+public class DisplayRefreshRateHelper {
+
+    private static final float DEFAULT_REFRESH_RATE = 60f;
+
+    private static DisplayRefreshRateHelper sInstance = null;
+
+    private final ArrayList<Integer> mRefreshRateList = new ArrayList<>();
+    private final Context mContext;
+
+    public static DisplayRefreshRateHelper getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new DisplayRefreshRateHelper(context);
+        }
+        return sInstance;
+    }
+
+    private DisplayRefreshRateHelper(Context context) {
+        mContext = context;
+        initialize();
+    }
+
+    private void initialize() {
+        final Display.Mode mode = mContext.getDisplay().getMode();
+        final Display.Mode[] modes = mContext.getDisplay().getSupportedModes();
+        for (Display.Mode m : modes) {
+            if (m.getPhysicalWidth() == mode.getPhysicalWidth() &&
+                    m.getPhysicalHeight() == mode.getPhysicalHeight()) {
+                mRefreshRateList.add((int) m.getRefreshRate());
+            }
+        }
+        mRefreshRateList.sort(Comparator.naturalOrder());
+    }
+
+    public ArrayList<Integer> getSupportedRefreshRateList() {
+        return mRefreshRateList;
+    }
+
+    public int getMinimumRefreshRate() {
+        final int refreshRate = mContext.getResources().getInteger(
+                R.integer.config_defaultRefreshRate);
+        final float defaultRefreshRate = refreshRate != 0 ? (float) refreshRate : DEFAULT_REFRESH_RATE;
+        final int ret = (int) Settings.System.getFloatForUser(mContext.getContentResolver(),
+                MIN_REFRESH_RATE, defaultRefreshRate, UserHandle.USER_SYSTEM);
+        if (mRefreshRateList.size() != 0 && !mRefreshRateList.contains(ret)) {
+            return mRefreshRateList.get(mRefreshRateList.size() - 1);
+        }
+        return ret;
+    }
+
+    public int getPeakRefreshRate() {
+        final int refreshRate = mContext.getResources().getInteger(
+                R.integer.config_defaultPeakRefreshRate);
+        final float defaultPeakRefreshRate = refreshRate != 0 ? (float) refreshRate : DEFAULT_REFRESH_RATE;
+        final int ret = (int) Settings.System.getFloatForUser(mContext.getContentResolver(),
+                PEAK_REFRESH_RATE, defaultPeakRefreshRate, UserHandle.USER_SYSTEM);
+        if (mRefreshRateList.size() != 0 && !mRefreshRateList.contains(ret)) {
+            return mRefreshRateList.get(mRefreshRateList.size() - 1);
+        }
+        return ret;
+    }
+
+    public ArrayList<Integer> getRefreshRate() {
+        final ArrayList<Integer> ret = new ArrayList<>();
+        ret.add(getMinimumRefreshRate());
+        ret.add(getPeakRefreshRate());
+        return ret;
+    }
+
+    public void setMinimumRefreshRate(int refreshRate) {
+        Settings.System.putFloatForUser(mContext.getContentResolver(),
+                MIN_REFRESH_RATE, (float) refreshRate, UserHandle.USER_SYSTEM);
+    }
+
+    public void setPeakRefreshRate(int refreshRate) {
+        Settings.System.putFloatForUser(mContext.getContentResolver(),
+                PEAK_REFRESH_RATE, (float) refreshRate, UserHandle.USER_SYSTEM);
+    }
+
+    public void setRefreshRate(int minRefreshRate, int peakRefreshRate) {
+        setMinimumRefreshRate(minRefreshRate);
+        setPeakRefreshRate(peakRefreshRate);
+    }
+
+    public boolean isRefreshRateValid(int refreshRate) {
+        return mRefreshRateList.contains(refreshRate);
+    }
+}

core/java/com/android/internal/util/evolution/FullscreenTaskStackChangeListener.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.evolution;
+
+import static android.app.ActivityTaskManager.INVALID_TASK_ID;
+import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
+
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.IActivityTaskManager;
+import android.app.TaskStackListener;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * TaskStackListener that ignores freeform / mini-window focuses change
+ *
+ * @hide
+ */
+public abstract class FullscreenTaskStackChangeListener extends TaskStackListener {
+
+    private static final String DEFAULT_TAG = "FullscreenTaskStackChangeListener";
+
+    private final ActivityManager mActivityManager;
+    private final ActivityTaskManager mActivityTaskManager;
+    private final IActivityTaskManager mIActivityTaskManager;
+
+    private final boolean mObserveActivityChange;
+
+    private boolean mDebug = false;
+    private String mDebugTag = DEFAULT_TAG;
+
+    private boolean mListening = false;
+
+    private String mTopPackage = "";
+    private String mTopActivity = "";
+    private int mTopTaskId = INVALID_TASK_ID;
+
+    public FullscreenTaskStackChangeListener(Context context) {
+        this(context, false);
+    }
+
+    public FullscreenTaskStackChangeListener(Context context, boolean observeActivity) {
+        mActivityManager = context.getSystemService(ActivityManager.class);
+        mActivityTaskManager = ActivityTaskManager.getInstance();
+        mIActivityTaskManager = ActivityTaskManager.getService();
+        mObserveActivityChange = observeActivity;
+    }
+
+    public void setDebug(boolean debug) {
+        mDebug = debug;
+    }
+
+    public void setDebugTag(String tag) {
+        mDebugTag = tag;
+    }
+
+    public void setListening(boolean listening) {
+        if (mListening != listening) {
+            if (listening) {
+                mActivityTaskManager.registerTaskStackListener(this);
+                mListening = true;
+            } else {
+                mActivityTaskManager.unregisterTaskStackListener(this);
+                mListening = false;
+            }
+        }
+    }
+
+    private void handleChange(String newPackageName, String newActivityName, int newTaskId) {
+        if (mTopPackage.equals(newPackageName) && mTopActivity.equals(newActivityName)) {
+            return;
+        }
+        mTopActivity = newActivityName;
+        if (!mObserveActivityChange && mTopPackage.equals(newPackageName)) {
+            return;
+        }
+        mTopPackage = newPackageName;
+        mTopTaskId = newTaskId;
+        if (mDebug) {
+            Log.d(mDebugTag, "Change: mTopPackage=" + mTopPackage
+                    + ", mTopActivity=" + mTopActivity
+                    + ", mTopTaskId=" + mTopTaskId);
+        }
+        onFullscreenTaskChanged(mTopPackage, mTopActivity, mTopTaskId);
+    }
+
+    @Override
+    public void onTaskStackChanged() {
+        forceCheck();
+    }
+
+    @Override
+    public void onTaskFocusChanged(int taskId, boolean focused) {
+        if (focused) {
+            forceCheck();
+        }
+    }
+
+    public void forceCheck() {
+        try {
+            final ActivityTaskManager.RootTaskInfo info = mIActivityTaskManager.getFocusedRootTaskInfo();
+            if (info == null) {
+                return;
+            }
+            if (info.getWindowingMode() == WINDOWING_MODE_FREEFORM) {
+                return;
+            }
+            final ComponentName topActivity = info.topActivity;
+            if (topActivity == null) {
+                return;
+            }
+            handleChange(topActivity.getPackageName(), topActivity.getClassName(), info.taskId);
+        } catch (RemoteException e) {}
+    }
+
+    public String getTopPackageName() {
+        return mTopPackage;
+    }
+
+    public String getTopActivityName() {
+        return mTopActivity;
+    }
+
+    public int getTopTaskId() {
+        return mTopTaskId;
+    }
+
+    public abstract void onFullscreenTaskChanged(
+            String packageName, String activityName, int taskId);
+}

core/java/org/evolution/display/IRefreshRateListener.aidl
@@ -1,0 +1,14 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.display;
+
+/** @hide */
+oneway interface IRefreshRateListener {
+
+    void onRequestedRefreshRate(int refreshRate);
+
+    void onRequestedMemcRefreshRate(int refreshRate);
+}

services/java/com/android/server/SystemServer.java
@@ -335,6 +335,8 @@ import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
+import com.android.server.EvolutionSystemExService;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -1722,6 +1724,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("WindowManagerServiceOnInitReady");
             wm.onInitReady();
+            t.traceEnd();
+
+            t.traceBegin("StartEvolutionSystemExService");
+            mSystemServiceManager.startService(EvolutionSystemExService.class);
             t.traceEnd();
 
             // Start receiving calls from SensorManager services. Start in a separate thread